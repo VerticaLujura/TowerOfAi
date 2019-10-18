8c8021eb0f9a09784acd6d9b77f71c5f2b54d47a
RYMND1875 <aiminzoku@gmail.com>
2019-10-18 10:44:30 +0900

    ver0.1

---
+++ TowerOfAi/Assets/RayTest/draft/Characters/CharactersController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharactersController : MonoBehaviour
+{
+    public List<GameObject> characterList;
+    public GameObject Unit1;
+    public GameObject Unit2;
+    public GameObject Unit3;
+    public GameObject Unit4;
+    void Awake()
+    {
+        Initial();
+    }
+
+    void Initial(){
+        void setList(GameObject go){
+            characterList.Add(go);
+        }
+        setList(Unit1);
+        setList(Unit2);
+        setList(Unit3);
+        setList(Unit4);
+    }
+}
---
+++ TowerOfAi/Assets/RayTest/draft/UIDirector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIDirector : MonoBehaviour
+{
+    public GameObject characters;
+    public GameObject choicedChara;
+    public List<GameObject> characterList;
+
+    void keyAct(KeyCode key,GameObject go){
+        if(Input.GetKeyDown(key)){
+            Instantiate(go);
+        }
+    }
+    
+    void Start()
+    {characterList=characters.GetComponent<CharactersController>().characterList;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        keyAct(KeyCode.Alpha1,characterList[0]);
+        keyAct(KeyCode.Alpha2,characterList[1]);
+        keyAct(KeyCode.Alpha3,characterList[2]);
+        keyAct(KeyCode.Alpha4,characterList[3]);
+    }
+}
