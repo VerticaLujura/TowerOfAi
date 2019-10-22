c72fb60c6934c2a5a3c6bad32cd096b7467b4184
RYMND1875 <aiminzoku@gmail.com>
2019-10-22 23:39:25 +0900

    UIrework
    UIをカメラベースに表示

--- TowerOfAi/Assets/RayTest/draft/Units/Unit.cs
+++ TowerOfAi/Assets/RayTest/draft/Units/Unit.cs
@@ -6,10 +6,11 @@ public class Unit : MonoBehaviour
 {
 public int hp;
 public int attack;
-public float moveSpeed=0.1f;
+public float moveSpeed=0.01f;
 
 void Move(){
     transform.Translate(new Vector3(moveSpeed,0,0));
+
 }
 void Start(){
     GameObject.Find("UnitsData").GetComponent<UnitsData>().AddRunningUnitsList(this.gameObject);
